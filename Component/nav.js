07ce60044c1b9f7a2cc2cdcceef9a74c648d32fc
Vyga <vygintaskirda@gmail.com>
2023-02-27 12:46:34 +0200

    Testing nav

--- Component/nav.js
+++ Component/nav.js
@@ -1,17 +1,17 @@
 let initNav=()=>{
-    
+
 let nav = document.createElement('ul') 
 nav.classList.add('nav')
 
 nav.innerHTML = `
     <li id="Home"><a href="../index.html">Home</a></li>
-    <li id="RefVal"><a href="/Pages/refValPage.html">Ref / Val</a></li>
-    <li id="RefVal"><a href="/Pages/scopePage.html">Scope</a></li>
+    <li id="RefVal"><a href="./refValPage.html">Ref / Val</a></li>
+    <li id="RefVal"><a href="./scopePage.html">Scope</a></li>
     <li id="Functions"><a href="/Pages/functionsPage.html">Functions</a></li>
     <li id="Call-Back"><a href="/Pages/callBackPage.html">Call Back</a></li>
     <li id="Promises"><a href="/Pages/promisesPage.html">Promises</a></li>
-    <li id="Fetch"><a href="/Pages/fetchPage.html">Fetch</a></li>
-    <li id="Fetch"><a href="/Pages/testPage.html">Test</a></li>
+    <li id="Fetch"><a href="./Pages/fetchPage.html">Fetch</a></li>
+    <li id="Fetch"><a href="/Pages/testPage">Test</a></li>
 `
 document.body.prepend(nav)
 }
