16be2bef3c5d9b66b009bd27e3c92503a6298e0d
Vyga <vygintaskirda@gmail.com>
2023-02-27 12:54:47 +0200

    Test Kind of found the problem.

--- Component/nav.js
+++ Component/nav.js
@@ -4,9 +4,9 @@ let nav = document.createElement('ul')
 nav.classList.add('nav')
 
 nav.innerHTML = `
-    <li id="Home"><a href="../index.html">Home</a></li>
-    <li id="RefVal"><a href="./refValPage.html">Ref / Val</a></li>
-    <li id="RefVal"><a href="./scopePage.html">Scope</a></li>
+    <li id="Home"><a href="../Back-to-basics-JS/index.html">Home</a></li>
+    <li id="RefVal"><a href="./Back-to-basics-JS/refValPage.html">Ref / Val</a></li>
+    <li id="RefVal"><a href="./Back-to-basics-JS/scopePage.html">Scope</a></li>
     <li id="Functions"><a href="/Pages/functionsPage.html">Functions</a></li>
     <li id="Call-Back"><a href="/Pages/callBackPage.html">Call Back</a></li>
     <li id="Promises"><a href="/Pages/zzzz">Promises</a></li>
