44d892fa95d60442373be58f22028c1b0ee2de34
Vyga <vygintaskirda@gmail.com>
2023-03-06 10:27:01 +0200

    Switch description  update

--- components/switchComp.js
+++ components/switchComp.js
@@ -6,12 +6,17 @@ const switchComp = () => {
 
     let switchDiv = createElement('div', ['switch', 'component'])
     switchDiv.innerHTML = `
-   <h1>Switch page:</h1>
-   <button class='changeBackgroundColor'> Switch background color</button>
-   <br>
-   <br>
-   <img src='./img/switch/switch1.png'></img>
-   `
+    <h1>Switch page:</h1>
+    <button class='changeBackgroundColor'> Switch background color</button>
+    <br>
+    <br>
+    <img src='./img/switch/switch1.png'/></img>
+    <ol>
+        <li>SWITCH is similar to IF. You give argument (input) to switch parameter and switch checks "input" value against each case. If case is a match switch initiate that case body code and breaks. If case is not found - default is initiated</li>
+        <li>In this case switch initiated when button is clicked.</li>
+        <li>In this case i replace initial value of INPUT to different value  so button would toggle through colors of each case every time button is clicked</li>
+    </ol>
+    `
     document.body.append(switchDiv)
 
 
